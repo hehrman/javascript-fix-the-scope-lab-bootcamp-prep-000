752a8e5a520cbb8a9c6b735c7e6b3cd21efc3971
hehrman@me.com Ehrman <hehrman@me.com>
2017-12-18 21:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ var funkyFunction = function() {
   }
 }
 
-var theFunk = funkyFunction()
-theFunk()
-"FUNKY!"
-
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+
